2c0573768e06500c27586bc53c34e6ee96fc8659
khush107 <veryhappyforyoumyfriend@gmail.com>
2023-06-25 14:34:43 +0530

    id js

---
+++ id.js
@@ -0,0 +1,6 @@
+const h1 = document.getElementById('title');
+h1.style.color = 'red';
+
+const btn = document.getElementById('btn');
+btn.style.backgroundColor = 'green';
+btn.style.color = "brown";
