f0bd32a4caaba16b45b0e5031a9efbb93db28eaa
khush107 <veryhappyforyoumyfriend@gmail.com>
2023-06-26 14:40:31 +0530

    use capture js

---
+++ usecapture.js
@@ -0,0 +1,6 @@
+document.querySelector("#inner").addEventListener("click", function(){
+    alert("inner div");
+},true);
+document.querySelector("#outer").addEventListener("click", function(){
+    alert("outer div");
+},true);
