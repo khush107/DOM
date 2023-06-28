90a0fb20df36feef1ae6e48be1cfdb2bfa740352
khush107 <veryhappyforyoumyfriend@gmail.com>
2023-06-28 17:39:09 +0530

    isequal js

---
+++ isequal.js
@@ -0,0 +1,4 @@
+var target1 = document.getElementById("list1").children[0];
+var target2 = document.getElementById("list2").children[0];
+var equal = target1.isEqualNode(target2);
+console.log(equal);
