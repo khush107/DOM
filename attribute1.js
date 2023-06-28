1209696a3a576cc6b7fa50bb9e90eed7c0008612
khush107 <veryhappyforyoumyfriend@gmail.com>
2023-06-28 17:36:46 +0530

    attribute js

---
+++ attribute1.js
@@ -0,0 +1,9 @@
+//hasAttributes
+// var target = document.getElementById("test");
+// var find = target.hasAttribute("class");
+// console.log(find);
+
+//hasChildNodes
+// var target = document.getElementById("test");
+// var find = target.hasChildNodes;
+// console.log(find);
