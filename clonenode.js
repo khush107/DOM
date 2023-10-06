ddd9c9599048214f70162c6c36e8f09fc6fd184f
khush107 <veryhappyforyoumyfriend@gmail.com>
2023-10-06 23:00:59 +0530

    edited

--- clonenode.js
+++ clonenode.js
@@ -1,4 +1,4 @@
 //cloneNode
 var target = document.getElementById("list1").children[0];
-var copyElement =  target.cloneNode(true);  //use for copy that element
+var copyElement =  target.cloneNode(true);  //use for copy that  element
 document.getElementById("list2").appendChild(copyElement);
